179dae56ba0630e860dbec3c2f5eb8add55c084e
Bindiya <bindiya.sameem@gmail.com>
2019-04-07 18:49:55 -0400

    finish first test

--- src/main/java/com/techelevator/DAO/PrimeNumberGenerator.java
+++ src/main/java/com/techelevator/DAO/PrimeNumberGenerator.java
@@ -1,16 +1,24 @@
 package com.techelevator.DAO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeNumberGenerator implements PrimeNumberGeneratorInterface{
 
 	@Override
 	public List<Integer> generate(int startValue, int endValue) {
-		return null;
+		List<Integer> listOfPrimeNumbers = new ArrayList<Integer>();
+		for(int i = startValue; i <= endValue; i++) {
+			if(i == 2) {
+				listOfPrimeNumbers.add(i);
+			}
+		}
+		return listOfPrimeNumbers;
 	}
 
 	@Override
 	public boolean isPrime(int value) {
+		
 		return false;
 	}
 
